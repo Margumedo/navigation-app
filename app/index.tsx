42bf6389f440005a656b6759b97cc4be84219fc8
Maicol Argumedo <margumedo.sm@gmail.com>
2024-12-23 01:51:35 -0400

    initial configuration

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+
+
+
+import { View, Text } from 'react-native'
+
+
+const App = () => {
+    return (
+        <View className='mt-10 bg-blue-900'>
+            <Text className='text-3xl font-bold text-blue-500'>Hola desde el Home</Text>
+        </View>
+    )
+}
+
+export default App
